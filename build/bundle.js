1f343c7eeb2cfeec21c79a97dacacf02f2ab7418
bystranovskiy <bystranovskiy@gmail.com>
2019-10-31 18:02:38 +0200

    responsive navbar is complete

--- build/bundle.js
+++ build/bundle.js
@@ -79,6 +79,27 @@ module.exports = __webpack_require__(1);
 
 __webpack_require__(2);
 
+(function ($) {
+
+  $('.mobile-menu-toggle').on('click', function () {
+    if ($(this).hasClass('return')) {
+      $(this).removeClass('return');
+      $('.mainmenu .submenu').removeClass('show');
+    } else {
+      $(this).toggleClass('active');
+      $('.navbar .mainmenu').toggleClass('show');
+    }
+  });
+
+  $('.mainmenu .has-submenu').on('click', function (e) {
+    if ($(window).width() < 768) {
+      e.preventDefault();
+      $('.mobile-menu-toggle').addClass('return');
+      $(this).next('.submenu').addClass('show');
+    }
+  });
+})(jQuery);
+
 /***/ }),
 /* 2 */
 /***/ (function(module, exports) {
--- src/js/entry.es6
+++ src/js/entry.es6
@@ -1,1 +1,25 @@
 require("../sass/style.scss");
+
+(function ($) {
+
+  $('.mobile-menu-toggle').on('click', function () {
+    if ($(this).hasClass('return')) {
+      $(this).removeClass('return');
+      $('.mainmenu .submenu').removeClass('show');
+    } else {
+      $(this).toggleClass('active');
+      $('.navbar .mainmenu').toggleClass('show');
+    }
+  });
+
+
+  $('.mainmenu .has-submenu').on('click', function (e) {
+    if ($(window).width() < 768) {
+      e.preventDefault();
+      $('.mobile-menu-toggle').addClass('return');
+      $(this).next('.submenu').addClass('show');
+    }
+  });
+
+
+})(jQuery);
